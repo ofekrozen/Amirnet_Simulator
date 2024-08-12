cc4e50fde9661d8f3b362bcd29becb6a9456fb28
ofekrozen <ofekool90@gmail.com>
2024-08-12 16:05:23 +0300

    Simulator design

--- Amirnet_Simulator/Simulator/urls.py
+++ Amirnet_Simulator/Simulator/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # path("login", views.login_view, name="login"),
+    path("simulator",views.start_simulator, name="simulator"),
+    # path("login", views.login, name="login"),
     # path("logout", views.logout_view, name="logout"),
     # path("register", views.register, name="register")
 ]
--- Amirnet_Simulator/Simulator/views.py
+++ Amirnet_Simulator/Simulator/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
+from random import randint
 
 # Create your views here.
 
 def index(request):
     return render(request,'Simulator/index.html')
+
+def login(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, 'Simulator/login.html')
+
+def start_simulator(request):
+    if request.method == "POST":
+        chapters = [{'order':1,'title':'Sentence Completion 1','questioncnt':4,'time':4},{'order':2,'title':'Sentence Completion 2','questioncnt':4,'time':4},{'order':3,'title':'Reading Comprehansion','questioncnt':5,'time':15},{'order':4,'title':'Restatement 1','questioncnt':3,'time':6},{'order':5,'title':'Restatement 2','questioncnt':3,'time':6},{'order':6,'title':'Sentence Completion 3','questioncnt':4,'time':4}]
+        test = []
+        questions = []
+        for j in range(2):
+            for i in range(4):
+                order = i+1
+                question = f'question {i+1}'
+                answers = {'1':'answer 1','2':'answer 2','3':'answer 3','4':'answer 4'}
+                correct = randint(1,4)
+                toappend = {'order':order,'question':question,'answers': answers,'correct':correct}
+                questions.append(toappend)
+
+            chapters[j]['questions'] = questions
+            questions = []
+        
+        return render(request,'Simulator/simulator.html',{
+            'chapters':chapters
+        })
+
+
+    else:
+        return render(request,'Simulator/simulator_prep.html')
